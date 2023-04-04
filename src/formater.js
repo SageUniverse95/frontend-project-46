039e8551168051d9c2394d7ffe130c680bf29a50
Andrey Krivoshein <industria8888@gmail.com>
2023-04-04 16:29:42 +0300

    test formatter default mode

--- src/formater.js
+++ src/formater.js
@@ -1,22 +1,20 @@
+import _ from 'lodash';
+
 const makeIndent = (depth, indent = ' ', numberOfIndents = 4, shiftLeft = 2) => {
   const result = (depth * numberOfIndents) - shiftLeft;
   return indent.repeat(result);
 };
 
-const stringfy = (value, replacer = ' ', spacesCount = 1) => {
-  const iter = (currentValue, counter) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return String(currentValue);
-    }
-    const indentSize = counter * spacesCount;
-    const currentIdent = replacer.repeat(indentSize);
-    const backetIdent = replacer.repeat(indentSize - spacesCount);
-    const arrOfObj = Object.entries(currentValue);
-    const lines = arrOfObj.map(([key, valueOfObj]) => `${currentIdent}${key}: ${iter(valueOfObj, counter + 1)}`);
-    const result = ['{', ...lines, `${backetIdent}}`].join('\n');
-    return result;
-  };
-  return iter(value, 1);
+const stringfy = (data, depth) => {
+  if (!_.isPlainObject(data) || data === null) {
+    return String(data);
+  }
+  const line = Object.entries(data).map(([key, value]) => `${makeIndent(depth + 1)}${key}: ${stringfy(value, depth + 1)}`);
+  return [
+    '{',
+    ...line,
+    `${makeIndent(depth)}}`,
+  ].join('\n');
 };
 
 const formatter = (dif) => {
@@ -26,20 +24,20 @@ const formatter = (dif) => {
         return `${makeIndent(depth)} ${data.key}: ${iter(data.child, depth + 1)}`;
       }
       if (data.type === 'deleted') {
-        return `${makeIndent(depth)}- ${data.key}: ${stringfy(data.value1)}`;
+        return `${makeIndent(depth)}- ${data.key}: ${stringfy(data.value1, depth)}`;
       }
       if (data.type === 'added') {
-        return `${makeIndent(depth)}+ ${data.key}: ${stringfy(data.value1)}`;
+        return `${makeIndent(depth)}+ ${data.key}: ${stringfy(data.value1, depth)}`;
       }
       if (data.type === 'changed') {
-        return `${makeIndent(depth)}- ${data.key}: ${stringfy(data.value1)}\n${makeIndent(depth)}+ ${data.key}: ${stringfy(data.value2)}`;
+        return `${makeIndent(depth)}- ${data.key}: ${stringfy(data.value1, depth)}\n${makeIndent(depth)}+ ${data.key}: ${stringfy(data.value2, depth)}`;
       }
       if (data.type === 'unchanged') {
-        return `${makeIndent(depth)}  ${data.key}: ${stringfy(data.value1)}`;
+        return `${makeIndent(depth)}  ${data.key}: ${stringfy(data.value1, depth)}`;
       }
-      return '';
+      return 'default message';
     });
-    const result = ['{', ...line, '}'].join('\n');
+    const result = ['{', ...line, `${makeIndent(depth)}}`].join('\n');
     return result;
   };
   return iter(dif, 1);
